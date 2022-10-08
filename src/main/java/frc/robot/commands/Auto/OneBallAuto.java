92943d4e346b75cc9af1140f986061c5d73f11a7
ntnx0246 <shawngazin@protonmail.com>
2022-10-08 00:31:35 -0700

    Created a new driving command auto

--- src/main/java/frc/robot/commands/Auto/OneBallAuto.java
+++ src/main/java/frc/robot/commands/Auto/OneBallAuto.java
@@ -7,13 +7,14 @@ import frc.robot.commands.DriveTrain.DriveStraight;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
+import frc.robot.commands.DriveTrain.Driving;
 
 public class OneBallAuto extends SequentialCommandGroup {
 
   public OneBallAuto(Intake intake, Shooter shooter, DriveTrain driveTrain) {
     addCommands(
         new WaitCommand(2),
-        new DriveStraight(driveTrain, -24).withTimeout(5));//,
+        new Driving(driveTrain, -30, 0.04));//,
         //new Shoot(intake, shooter, -2600, 1000).withTimeout(5),
         //new DriveStraight(driveTrain, () -> -100).withTimeout(5));
   }
