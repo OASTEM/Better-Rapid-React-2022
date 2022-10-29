a10ac79c4155f7b6d5c40ce75d8620e95bff8191
FRC Member <>
2022-10-29 10:42:52 -0700

    reverted autoclimb

--- src/main/java/frc/robot/commands/Climber/AutoClimb.java
+++ src/main/java/frc/robot/commands/Climber/AutoClimb.java
@@ -17,22 +17,16 @@ public class AutoClimb extends SequentialCommandGroup {
         new InstantCommand(() -> pivots.setPositionPivots(-41, Constants.Pivot.FAST_PID.s)).withTimeout(2),
         new ClimbDown(climber, 1000, 1000).withTimeout(2)
             .andThen(new PivotRelative(pivots, 15, Constants.Pivot.FAST_PID.s).withTimeout(1)),
-            // .andThen(new PivotRelative(pivots, 20, Constants.Pivot.FAST_PID.s).withTimeout(1)),
+        // .andThen(new PivotRelative(pivots, 20,
+        // Constants.Pivot.FAST_PID.s).withTimeout(1)),
         new ClimbUp(climber, 55000, 55000, Constants.Climber.UP.s).withTimeout(2)
-            .alongWith(new InstantCommand(() -> pivots.setPositionPivots(-37, Constants.Pivot.FAST_PID.s))).withTimeout(2)
+            .alongWith(new InstantCommand(() -> pivots.setPositionPivots(-37, Constants.Pivot.FAST_PID.s)))
+            .withTimeout(2)
             .andThen(new PivotRelative(pivots, -15, Constants.Pivot.FAST_PID.s).withTimeout(1)),
         new PivotRelative(pivots, 90, Constants.Pivot.SLOW_PID.s).withTimeout(1.2)
             .alongWith(new ClimbUp(climber, Constants.Climber.TURBO.s).withTimeout(1.2)),
         // new WaitCommand(1),
         new PivotRelative(pivots, -25, Constants.Pivot.MODERATE_PID.s).withTimeout(1.5),
-        new PivotRelative(pivots, 10, Constants.Pivot.FAST_PID.s).withTimeout(.7),
-        new PivotRelative(pivots, -10, Constants.Pivot.MODERATE_PID.s).withTimeout(1.5),
-        new PivotRelative(pivots, 10, Constants.Pivot.FAST_PID.s).withTimeout(.7),
-        new PivotRelative(pivots, -10, Constants.Pivot.MODERATE_PID.s).withTimeout(1.5),
-        new PivotRelative(pivots, 10, Constants.Pivot.FAST_PID.s).withTimeout(.7),
-        new PivotRelative(pivots, -10, Constants.Pivot.MODERATE_PID.s).withTimeout(1.5),
-        new PivotRelative(pivots, 10, Constants.Pivot.FAST_PID.s).withTimeout(.7),
-        new PivotRelative(pivots, -10, Constants.Pivot.MODERATE_PID.s).withTimeout(1.5),
         new PivotRelative(pivots, 10, Constants.Pivot.FAST_PID.s).withTimeout(.7),
         new PivotRelative(pivots, -10, Constants.Pivot.MODERATE_PID.s).withTimeout(1.5),
         // new WaitCommand(1),
@@ -41,7 +35,6 @@ public class AutoClimb extends SequentialCommandGroup {
         new PivotRelative(pivots, -95, Constants.Pivot.FAST_PID.s).withTimeout(1),
         new ClimbDown(climber, 100000, 100000).withTimeout(1.5),
         new InstantCommand(() -> pivots.setPositionPivots(-41, Constants.Pivot.FAST_PID.s)).withTimeout(1),
-        new WaitCommand(2)
-    );
+        new WaitCommand(2));
   }
 }
