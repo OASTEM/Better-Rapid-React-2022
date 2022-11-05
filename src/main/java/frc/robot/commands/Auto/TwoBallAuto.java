4c8b44647824571aafa02cd331ff581246fe1ad6
FRC Member <>
2022-11-05 03:39:03 -0700

    twoballauto update

--- src/main/java/frc/robot/commands/Auto/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/Auto/TwoBallAuto.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.IntakeBalls;
 import frc.robot.commands.Shoot;
 import frc.robot.commands.DriveTrain.DriveStraight;
+import frc.robot.commands.DriveTrain.Driving;
 import frc.robot.commands.DriveTrain.TurnToAngle;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Intake;
@@ -20,13 +21,14 @@ public class TwoBallAuto extends SequentialCommandGroup {
   public TwoBallAuto(Intake intake, Shooter shooter, DriveTrain driveTrain, NavX navX, double angle,
       Limelight limelight) {
     addCommands(
-        new Shoot(intake, shooter, limelight, -3000, 1200, driveTrain).withTimeout(5),
+      new Driving(driveTrain, -70, 0.014),
+        new Shoot(intake, shooter, limelight, -3000, 1200, driveTrain).withTimeout(6),
         new TurnToAngle(driveTrain, navX, angle).withTimeout(2),
         new DriveStraight(driveTrain, 45).withTimeout(2).alongWith(
-            new IntakeBalls(intake).withTimeout(2)),
+        new IntakeBalls(intake).withTimeout(2)),
         new DriveStraight(driveTrain, -45).withTimeout(2),
         new TurnToAngle(driveTrain, navX, -angle).withTimeout(2),
         new Shoot(intake, shooter, limelight, -3000, 1200, driveTrain).withTimeout(4),
         new DriveStraight(driveTrain, -50).withTimeout(3));
   }
-}
+} 
